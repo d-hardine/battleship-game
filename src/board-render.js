a01950e04302ce451c4ac550fbe8377360e8559d
Dhiva Ryan H <176643369+d-hardine@users.noreply.github.com>
2025-03-14 10:39:32 +0700

    create clickable blocks to attack the enemy

--- src/board-render.js
+++ src/board-render.js
@@ -13,7 +13,6 @@ function createBoard(user) {
 
     for(let i=0; i < 100; i++) {
         const block = document.createElement('div')
-        //block.classList.add('none')
         if(user.GameBoard.boardInfo[i] == 'none')
             block.classList.add('none')
         else
@@ -28,4 +27,32 @@ function createBoard(user) {
 createBoard(player)
 createBoard(enemy)
 
-console.log(player.GameBoard.boardInfo)
+//create clickable blocks
+const playerBlocks = document.querySelectorAll('#player div')
+const enemyBlocks = document.querySelectorAll('#enemy div')
+
+function clickableBlocks(user, userBlocks) {
+    let randomNumberArray = []
+    userBlocks.forEach(userBlocks => {
+        userBlocks.addEventListener('click', () =>  {
+            user.GameBoard.receiveAttack(userBlocks.id)
+            userBlocks.setAttribute('class', '')
+            userBlocks.classList.add(user.GameBoard.boardInfo[userBlocks.id])
+        
+            //enemy attack to player
+            let randomNumber = Math.floor(Math.random() * 100)
+
+            while(randomNumberArray.includes(randomNumber)) //enemy will never attack the same location twice
+                randomNumber = Math.floor(Math.random() * 100)
+            randomNumberArray.push(randomNumber)
+
+            console.log('enemy attack:', randomNumberArray)
+            player.GameBoard.receiveAttack(randomNumber)
+            playerBlocks[randomNumber].setAttribute('class', '')
+            playerBlocks[randomNumber].classList.add(player.GameBoard.boardInfo[randomNumber])
+        })
+    })
+}
+
+clickableBlocks(player, playerBlocks)
+clickableBlocks(enemy, enemyBlocks)
