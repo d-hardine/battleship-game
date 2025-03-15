87f4f34cc14880943afcef566dd1fae1fdf67fc1
Dhiva Ryan H <176643369+d-hardine@users.noreply.github.com>
2025-03-15 13:42:39 +0700

    initialize the players, boards, and hide the enemy ships

--- src/index.js
+++ src/index.js
@@ -1,5 +1,20 @@
 import './styles.css'
-import { greeting } from './battleship.js'
-import { jancok } from './board-render.js'
+import { player, enemy } from "./battleship"
+import { createBoard, clickableBlocks } from './board-render.js'
 
-console.log(greeting, jancok)
+//create player and enemy boards
+createBoard(player)
+createBoard(enemy)
+
+//create clickable blocks inside boards
+const playerBlocks = document.querySelectorAll('#player div')
+const enemyBlocks = document.querySelectorAll('#enemy div')
+
+let test = clickableBlocks(enemy, enemyBlocks, player, playerBlocks) //playerBlocks is inserted to make sure the enemy can attack the player blocks
+console.log(test)
+
+//hide the enemy's ship placement
+enemyBlocks.forEach(enemyBlock => {
+    enemyBlock.setAttribute('class', '')
+    enemyBlock.classList.add('hidden')
+})
